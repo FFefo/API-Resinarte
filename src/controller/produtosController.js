3badaa48c894f0ac5e1a703e29aba26b009e0c0d
FFefo <felpo.correiacosta@gmail.com>
2024-10-31 03:01:07 -0300

    endpoint de busca por id + categoria

--- src/controller/produtosController.js
+++ src/controller/produtosController.js
@@ -5,18 +5,18 @@ import { autenticar } from '../utils/jwt.js';
 const endpoints = Router();
 
 endpoints.get('/produtos/', async (req, resp) => {
-    try{
+    try {
         let registros = await db.consultarProdutos();
         resp.send(registros);
     }
-    catch(err){
+    catch (err) {
         resp.status(400).send({
             erro: err.message
         })
     }
 })
 
-endpoints.get('/produtos/:id', async (req, resp) =>{
+endpoints.get('/produtos/:id', autenticar, async (req, resp) => {
     try {
         let id = req.params.id
 
@@ -29,7 +29,7 @@ endpoints.get('/produtos/:id', async (req, resp) =>{
     }
 })
 
-endpoints.get('/produto/:categoria', autenticar, async (req, resp) =>{
+endpoints.get('/produto/:categoria', async (req, resp) => {
     try {
         let categoria = req.params.categoria
 
@@ -42,18 +42,32 @@ endpoints.get('/produto/:categoria', autenticar, async (req, resp) =>{
     }
 })
 
+endpoints.get('/produtos/:id/:categoria', async (req, resp) => {
+    try {
+        let id = req.params.id
+        let categoria = req.params.categoria
+
+        let registros = await db.consultarProdutosPorCategoriaId(id, categoria);
+        resp.send(registros);
+    } catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
 endpoints.post('/produtos/', autenticar, async (req, resp) => {
-    try{
+    try {
         let produto = req.body
         produto.idUsuario = req.user.id
 
         let id = await db.inserirProduto(produto);
-        
+
         resp.send({
             novoId: id
         })
     }
-    catch(err){
+    catch (err) {
         resp.status(400).send({
             erro: err.message
         })
@@ -61,19 +75,19 @@ endpoints.post('/produtos/', autenticar, async (req, resp) => {
 })
 
 endpoints.put('/produtos/:id', autenticar, async (req, resp) => {
-    try{
+    try {
         let id = req.params.id;
         let produto = req.body;
 
         let linhasAfetadas = await db.alterarProduto(id, produto)
-        if (linhasAfetadas >= 1){
+        if (linhasAfetadas >= 1) {
             resp.send();
         }
-        else{
-            resp.status(400).send({erro: 'Nenhum registro encontrado.'})
+        else {
+            resp.status(400).send({ erro: 'Nenhum registro encontrado.' })
         }
     }
-    catch(err){
+    catch (err) {
         resp.status(400).send({
             erro: err.message
         })
@@ -81,18 +95,18 @@ endpoints.put('/produtos/:id', autenticar, async (req, resp) => {
 })
 
 endpoints.delete('/produtos/:id', autenticar, async (req, resp) => {
-    try{
+    try {
         let id = req.params.id;
 
         let linhasAfetadas = await db.removerProduto(id);
-        if (linhasAfetadas >= 1){
-            resp.send();
+        if (linhasAfetadas >= 1) {
+            resp.send(`${linhasAfetadas}`);
         }
-        else{
-            resp.status(400).send({erro:'Nenhum registro encontrado.'})
+        else {
+            resp.status(400).send({ erro: 'Nenhum registro encontrado.' })
         }
     }
-    catch(err){
+    catch (err) {
         resp.status(400).send({
             erro: err.message
         })
--- src/repository/produtosRepository.js
+++ src/repository/produtosRepository.js
@@ -33,41 +33,40 @@ export async function consultarProdutos() {
 
 export async function consultarProdutosPorID(id) {
     const comando = `
-        select id_produto       id,
-               nm_produto       produto,
-               ds_produto       descricao,
-               ds_categoria     categoria,
-               qtd_produto      quantidade,
-               vl_preco         preco,
-               img_produto      imagem,
-               id_usuario       usuario
-         from tb_produtos
-         where id_produto = ?;
+    select id_produto       id,
+	       nm_produto       produto,
+	       ds_produto       descricao,
+	       ds_categoria     categoria,
+	       qtd_produto      quantidade,
+	       vl_preco         preco,
+	       img_produto      imagem,
+	       id_usuario       usuario
+    from tb_produtos
+    where id_produto = ?
     `
 
     let resposta = await con.query(comando, [id]);
     let registros = resposta[0][0];
 
-    if(registros.imagem != null){
+    if (registros.imagem != null) {
         registros.imagem = registros.imagem.toString();
     }
 
     return registros;
 }
 
-
 export async function consultarProdutosPorCategoria(categoria) {
     const comando = `
-        select id_produto       id,
-               nm_produto       produto,
-               ds_produto       descricao,
-               ds_categoria     categoria,
-               qtd_produto      quantidade,
-               vl_preco         preco,
-               img_produto      imagem,
-               id_usuario       usuario
-         from tb_produtos
-         where ds_categoria = ?;
+    select id_produto       id,
+	       nm_produto       produto,
+	       ds_produto       descricao,
+	       ds_categoria     categoria,
+	       qtd_produto      quantidade,
+	       vl_preco         preco,
+	       img_produto      imagem,
+	       id_usuario       usuario
+    from tb_produtos
+    where ds_categoria = ?;
     `
 
     let resposta = await con.query(comando, [categoria]);
@@ -80,11 +79,35 @@ export async function consultarProdutosPorCategoria(categoria) {
     return registros;
 }
 
+export async function consultarProdutosPorCategoriaId(id, categoria) {
+    const comando = `
+    select id_produto       id,
+	       nm_produto       produto,
+	       ds_produto       descricao,
+	       ds_categoria     categoria,
+	       qtd_produto      quantidade,
+	       vl_preco         preco,
+	       img_produto      imagem,
+	       id_usuario       usuario
+    from tb_produtos
+    where id_produto = ? and
+    ds_categoria = ?;
+    `
+
+    let resposta = await con.query(comando, [id, categoria]);
+    let registros = resposta[0][0];
+
+    if (registros.imagem != null) {
+        registros.imagem = registros.imagem.toString();
+    }
+
+    return registros;
+}
+
 export async function alterarProduto(id, produto) {
     const comando = `
     update tb_produtos
-    set id_produto = ?,
-        nm_produto = ?,
+    set nm_produto = ?,
         ds_produto = ?,
         ds_categoria = ?,
         qtd_produto = ?,
@@ -92,9 +115,9 @@ export async function alterarProduto(id, produto) {
         img_produto = ?,
         id_usuario = ?
     where id_produto = ?
-`;
+    `;
 
-    let resposta = await con.query(comando, [produto.nome, produto.descricao, produto.categoria, produto.quantidade, produto.preco, produto.imagem, id]);
+    let resposta = await con.query(comando, [produto.nome, produto.descricao, produto.categoria, produto.quantidade, produto.preco, produto.imagem,, produto.usuario, id]);
 
     let info = resposta[0];
 
